d472723227f453eaa989c6a6c7e69c2a32f2cda0
hammadmazhar1 <16100032@lums.edu.pk>
2014-03-29 15:12:53 +0500

    Method definitions
    Defined methods in Member class and added interface Status

--- Member.java
+++ Member.java
@@ -1,5 +1,5 @@
 import java.util.*
-public class Member {
+public class Member implements Status {
 	private String name;
 	private int level;
 	private int maxHp;
@@ -16,6 +16,7 @@ public class Member {
 	private Job job;
 	private ArrayList<AbilitySet> skills;
 	private Weapon eqWeapon;
+	private Shield eqShield;
 	private Armor eqArmor;
 	private Headgear eqHat;
 	private Accessory eqAcc;
@@ -40,6 +41,7 @@ public class Member {
 		res = job.intRes;
 		exp = 0;
 		eqWeapon = null;
+		eqShield = null;
 		eqArmor = null;
 		eqHat = null;
 		eqAcc = null;
@@ -66,6 +68,7 @@ public class Member {
 		res = chr.res;
 		exp = chr.exp;
 		eqWeapon = chr.eqWeapon;
+		eqShield = chr.eqShield;
 		eqArmor = chr.eqArmor;
 		eqHat = chr.eqHat;
 		eqAcc = chr.eqAcc;
@@ -82,59 +85,180 @@ public class Member {
 	public int getmaxMp() {
 		return maxMp
 	}
+	public int getLevel() {
+		return level;
+	}
 	public int getAtk() {
-		return atk+eqWeapon.getAtk()+job.getAtkBon();
+		if (eqWeapon) {
+			return atk + eqWeapon.getAtk();
+		} else {
+			return atk + job.getAtkBon();
+		}
 	}
 	public int getDef() {
+		if (eqArmor) {
+			return def + eqArmor.getDef();
+		} else {
+			return def;
+		}
 	}
 	public int getMove() {
+		if (eqAcc) {
+			return move + eqAcc.getMove();
+		} else {
+			return move;
+		}
 	}
 	public int getJump() {
+		if (eqAcc) {
+			return jmp + eqAcc.getJmp();
+		} else {
+			return jmp;
+		}
+	}
+	public int getMgc() {
+		int ans = mgc; 
+		if (eqAcc) {
+			ans += eqAcc.getMgc();
+		}
+		if (eqWeapon) {
+			ans += eqWeapon.getMgc();
+		}
+		return ans;
 	}
 	public int getEvade() {
+		int ans = evade;
+		if (eqAcc) {
+			ans+=eqAcc.getEvade();
+		}
+		if (eqArmor) {
+			ans += eqArmor.getEvade();
+		}
+		if (eqHat) {
+			ans += eqHat.getEvade();
+		}
+		if (eqShield) {
+			ans+= eqShield.getEvade();
+		}
+		if (eqWeapon) {
+			ans += eqWeapon.getEvade();
+		}
+		return ans;
 	}
-	public int getSpeed() {
+	public int getExp() {
+		return exp;
+	}
+	public int addExp(Member vic) {
+		int ans = 0;
+		if (level == vic.getLevel()) {
+			 ans = 10;
+		} else if (level+10 < vic.getLevel()) {
+			ans = 20;
+		} else if (vic.getLevel()+10 < level) {
+			ans = 1;
+		} else {
+			ans = 10 + (level - vic.getLevel());
+		}
+		exp += ans; 
+		return ans;
+	}
+	public int getSpd() {
+		int ans = speed;
+		if (eqAcc) {
+			ans += eqAcc.getSpd();
+		}
+		return ans;
 	}
 	public int getResist() {
+		int ans = res;
+		if (eqArmor) {
+			ans = eqArmor.getResist();
+		}
+		if (eqHat) {
+			ans = eqHat.getResist();
+		}
+		if (eqShield) {
+			ans = eqShield.getResist();
+		}
+		return ans;
 	}
 	public Job getJob() {
+		return job;
 	}
-	public boolean setJob(Job jb) {}
+	public boolean setJob(Job jb) {
+	}
 	public Weapon getWeap() {
+		return eqWeapon;
 	}
 	public boolean setWeap(Weapon wpn) {
 	}
 	public Armor getArmor() {
+		return eqArmor;
 	}
 	public boolean setArmor(Armor amr) {
 	}
 	public Headgear getHat() {
+		return eqHat;
 	}
 	public boolean setHat(Headgear hg) {
 	}
 	public Accessory getAcc() {
+		return eqAcc;
 	}
 	public boolean setAcc(Accessory acc) {
 	}
 	public ArrayList<AbilitySet> getSkills() {
+		return skills;
 	}
 	public PasAbil getReact() {
+		return reactAbil;
 	}
 	public boolean setReact(PasAbil pa) {
+		if (pa.reactive) {
+			for (AbilitySet i : skills) {
+				if (i.contains(pa)) {
+					if (pa.aquired()) {
+						reactAbil = pa;
+						return true;
+					} else {
+						if (eqWeapon.hasAbil(pa) || eqArmor.hasAbil(pa)) {
+							reactAbil = pa;
+						}
+					}
+				}
+			}
+		}
+		return false;
+		
 	}
 	public PasAbil getSupp() {
+		return setSupp;
 	}
 	public boolean setSupp(PasAbil pa) {
 	}
 	public AbilitySet getPrimAbil() {
+		return primAbil;
 	}
 	public boolean setPrimAbil(AbilitySet abil){
 	}
 	public AbilitySet getSecAbil() {
+		return secAbil;
 	}
 	public boolean setSecAbil(AbilitySet abil){
 	}
 	public boolean levelUp() {
+		exp -= 100;
+		atk += ((job.getAtkF()*4)+9)/10;
+		def += ((job.getDefF()*4)+9)/10;
+		mgc += ((job.getMgcF()*4)+9)/10;
+		Random r = new Random();
+		if (r.getDouble() <= job.getSpdF()) {
+			spd++;
+		}
+		maxHp = ((job.getHpF()*4)+9)/10;
+		maxMp = ((job.getMpF()*4)+9)/10;
+		res += ((job.getResF()*4)+9)/10;
+		
 	}
 
 }
---
+++ Status.java
@@ -0,0 +1,10 @@
+public interface Status {
+	public int getAtk();
+	public int getDef();
+	public int getMove();
+	public int getJmp();
+	public int getMgc();
+	public int getEvade();
+	public int getSpd();
+	public int getResist();
+}
